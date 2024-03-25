26a5477046132af878fe01f2974be172b51f2c65
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-25 19:52:38 +0900

    style: 비디오 위 투명 배경색 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import "./App.css"; // CSS 파일 임포트
 function App() {
   return (
     <div className="App">
-      <video className="body-overlay" autoPlay loop muted playsInline>
+      <div className="background-overlay"></div>
+      <video className="background" autoPlay loop muted playsInline>
         <source src="/public/assets/background.mp4" type="video/mp4" />
       </video>
     </div>
