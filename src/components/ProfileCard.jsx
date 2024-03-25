24fcbb0949334cd635243853793d126f20f92ace
Song Yu Jeong <udeanglvup@gmail.com>
2024-03-26 01:28:34 +0900

    깃허브, 티스토리, 이메일 아이콘 배치/ 깃허브 티스토리 링크 연결

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
 import profileImg from "../../public/assets/profile.png";
-
+import { SiTistory } from "react-icons/si";
+import { BsGithub } from "react-icons/bs";
+import { MdMail } from "react-icons/md";
 const ProfileCard = () => {
+  const handleGithubClick = () => {
+    window.open("https://github.com/udaeng8286", "_blank");
+  };
+
+  const handleTistoryClick = () => {
+    window.open("https://growdaeng.tistory.com/", "_blank");
+  };
+
+  const handleMailClick = () => {};
+
   return (
     <Container>
       <Name>Yu Jeong</Name>
       <Role>Front-end Development</Role>
       <ProfileImg src={profileImg} alt="profile Image" />
       <Email>udaenglvup@gmail.com</Email>
+      <ButtonSection>
+        <IconWrapper onClick={handleGithubClick}>
+          <BsGithub size={24} />
+        </IconWrapper>
+        <IconWrapper onClick={handleTistoryClick}>
+          <SiTistory size={16} />
+        </IconWrapper>
+        <IconWrapper onClick={handleMailClick}>
+          <MdMail size={24} />
+        </IconWrapper>
+      </ButtonSection>
     </Container>
   );
 };
@@ -15,13 +38,14 @@ export default ProfileCard;
 
 const Container = styled.div`
   padding: 2rem;
-  width: 320px;
+  width: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
   border-radius: 20px;
   border: 2px solid #cecece;
   box-shadow: 2px 2px 12px 4px rgba(0, 0, 0, 0.5);
+  gap: 0.5rem;
 `;
 
 const Name = styled.text`
@@ -39,4 +63,25 @@ const ProfileImg = styled.img`
 
 const Email = styled.text`
   font-size: 20px;
+`;
+
+const ButtonSection = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: 2px solid white;
+  margin-top: 1rem;
+  &:hover {
+    background-color: #ffffff80;
+  }
 `;
