7e03f0e683663921dbe5f507a36a48166e451e52
Song Yu Jeong <udeanglvup@gmail.com>
2024-04-25 17:25:41 +0900

    feat:도넛차트 애니메이션 추가

--- src/components/DonutChart.jsx
+++ src/components/DonutChart.jsx
@@ -1,36 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 const DonutChart = ({ value }) => {
-  const radius = 64;
-  const strokeWidth = 15;
-  const circumference = 2 * Math.PI * radius;
-  const progress = value / 100;
-  const offset = circumference * (1 - progress);
+  const [dashArray, setDashArray] = useState("0 400");
+
+  useEffect(() => {
+    const circumference = 2 * Math.PI * 64;
+    const progress = value / 100;
+    const filledLength = circumference * progress;
+    const emptyLength = circumference - filledLength;
+    setDashArray(`${filledLength} ${emptyLength}`);
+  }, [value]);
 
   return (
-    <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-      <circle
-        cx="100"
-        cy="100"
-        r={radius}
-        fill="rgba(243, 243, 243, 0.5)"
-        stroke=" none"
-        strokeWidth={strokeWidth}
-      />
-      <circle
-        cx="100"
-        cy="100"
-        r={radius}
-        fill="none"
-        stroke=" #e8c964"
-        strokeWidth={strokeWidth}
-        strokeDasharray={circumference}
-        strokeDashoffset={offset}
-        strokeLinecap="round"
-        transform="rotate(-90 100 100)"
-      />
-    </svg>
+    <Svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+      <g transform="rotate(-90, 100, 100)">
+        {" "}
+        {/* 시계 방향으로 90도 회전 */}
+        <circle
+          cx="100"
+          cy="100"
+          r="64"
+          fill="rgba(243, 243, 243, 0.5)"
+          stroke="none"
+          strokeWidth="15"
+        />
+        <Circle
+          cx="100"
+          cy="100"
+          r="64"
+          fill="none"
+          stroke="#e8c964"
+          strokeWidth="15"
+          strokeDasharray={dashArray}
+          strokeLinecap="round"
+        />
+      </g>
+    </Svg>
   );
 };
 
 export default DonutChart;
+
+const Svg = styled.svg`
+  width: 160px;
+  height: 160px;
+`;
+
+const Circle = styled.circle`
+  transition: stroke-dasharray 1s ease-in-out;
+`;
